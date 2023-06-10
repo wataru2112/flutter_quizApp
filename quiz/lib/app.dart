087d16f0e87f54ddb7dc3a0c6795e6f0d2b4222e
渡邉　晴哉 <watanabeha@systena.co.jp>
2023-06-11 08:55:13 +0900

    Flutter & Dart - The Complete Guide [2023 Edition]_20230611 #54

--- quiz/lib/app.dart
+++ quiz/lib/app.dart
@@ -8,8 +8,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: StartPage(),
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 88, 27, 193),
+                Color.fromARGB(255, 129, 84, 207),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartPage(),
+        ),
+      ),
     );
   }
 }
--- quiz/lib/view/start_page.dart
+++ quiz/lib/view/start_page.dart
@@ -5,41 +5,36 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.deepPurple),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 300,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                'Learn Fullter title fun way!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  primary: Colors.white,
-                  side: const BorderSide(color: Colors.black),
-                ),
-                child: const Text('StartQuiz'),
-              ),
-            ],
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
           ),
-        ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Fullter title fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 228, 201, 232),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              side: const BorderSide(color: Colors.black),
+            ),
+            child: const Text('StartQuiz'),
+          ),
+        ],
       ),
     );
   }
